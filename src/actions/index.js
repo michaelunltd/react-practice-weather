d7e1c10e36f1d8fdef8277a9cb64aba11f7eba40
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2017-06-03 20:22:33 +0800

    initial commit for react-redux-practice

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'a29e77611e0155f4b591bff17252084e';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
